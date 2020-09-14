3ca7ac30ff4a24bc19fb40175c174ecc6851004c
Andy Taylor <a.taylor@kainos.com>
2020-09-14 16:32:25 +0100

    Implement eslint by extending from dvsa/eslint-config-ts plugin

--- src/util/logger.ts
+++ src/util/logger.ts
@@ -1,33 +1,33 @@
-import { APIGatewayProxyEvent, Context } from "aws-lambda";
+import type { APIGatewayProxyEvent, Context } from 'aws-lambda';
 
 export class Logger {
-    logFormat: string;
+  logFormat: string;
 
-    constructor(apiRequestId: string, correlationId: string) {
-        this.logFormat = JSON.stringify({ apiRequestId: apiRequestId, correlationId: correlationId, message: "%s" });
-    }
+  constructor(apiRequestId: string, correlationId: string) {
+    this.logFormat = JSON.stringify({ apiRequestId, correlationId, message: '%s' });
+  }
 
-    public debug(msg: string): void { 
-        console.debug(this.logFormat, msg);
-    }
+  public debug(msg: string): void {
+    console.debug(this.logFormat, msg);
+  }
 
-    public info(msg: string): void { 
-        console.info(this.logFormat, msg);
-    }
+  public info(msg: string): void {
+    console.info(this.logFormat, msg);
+  }
 
-    public warn(msg: string): void { 
-        console.warn(this.logFormat, msg);
-    }
-    
-    public error(msg: string): void { 
-        console.error(this.logFormat, msg);
-    }
+  public warn(msg: string): void {
+    console.warn(this.logFormat, msg);
+  }
+
+  public error(msg: string): void {
+    console.error(this.logFormat, msg);
+  }
 }
 
 export const createLogger = (event: APIGatewayProxyEvent, context: Context): Logger => {
-    const lambdaRequestId: string = context.awsRequestId;
-    const apiRequestId: string = event.requestContext.requestId;
-    const correlationId: string =  event.headers["X-Correlation-Id"] || lambdaRequestId;
+  const lambdaRequestId: string = context.awsRequestId;
+  const apiRequestId: string = event.requestContext.requestId;
+  const correlationId: string = event.headers['X-Correlation-Id'] || lambdaRequestId;
 
-    return new Logger(apiRequestId, correlationId);
+  return new Logger(apiRequestId, correlationId);
 };
--- tests/util/logger.test.ts
+++ tests/util/logger.test.ts
@@ -1,69 +1,73 @@
-import { APIGatewayProxyEvent, Context, APIGatewayEventRequestContext } from "aws-lambda";
-import { v4 } from "uuid";
-import { createLogger, Logger } from "../../src/util/logger";
+import type { APIGatewayProxyEvent, Context, APIGatewayEventRequestContext } from 'aws-lambda';
+import { v4 } from 'uuid';
+import { createLogger, Logger } from '../../src/util/logger';
 
-describe("Test logger", () => {
-    test("createLogger() via context lambdaId should return a logger object with the correct logFormat", () => {
-        const queryStringParameters: Record<string, string> = { message: "Hello world!" };
-        const apiRequestId = v4();
-        const requestContext: APIGatewayEventRequestContext = <APIGatewayEventRequestContext> { requestId: apiRequestId };
-        const headers: Record<string, string> = {};
-        const eventMock: APIGatewayProxyEvent = <APIGatewayProxyEvent> { queryStringParameters,  requestContext, headers };
-        const awsRequestId = v4();
-        const contextMock: Context = <Context> { awsRequestId: awsRequestId };
+describe('Test logger', () => {
+  test('createLogger() via context lambdaId should return a logger object with the correct logFormat', () => {
+    const queryStringParameters: Record<string, string> = { message: 'Hello world!' };
+    const apiRequestId: string = v4();
+    const requestContext: APIGatewayEventRequestContext = <APIGatewayEventRequestContext> { requestId: apiRequestId };
+    const headers: Record<string, string> = {};
+    const eventMock: APIGatewayProxyEvent = <APIGatewayProxyEvent> { queryStringParameters, requestContext, headers };
+    const awsRequestId: string = v4();
+    const contextMock: Context = <Context> { awsRequestId };
 
-        const logger: Logger = createLogger(eventMock, contextMock);
+    const logger: Logger = createLogger(eventMock, contextMock);
 
-        expect(logger.logFormat).toBe(`{"apiRequestId":"${apiRequestId}","correlationId":"${awsRequestId}","message":"%s"}`);
-    });
+    expect(logger.logFormat).toBe(
+      `{"apiRequestId":"${apiRequestId}","correlationId":"${awsRequestId}","message":"%s"}`,
+    );
+  });
 
-    test("createLogger() via header should return a logger object with the correct logFormat", () => {
-        const queryStringParameters: Record<string, string> = { message: "Hello world!" };
-        const apiRequestId = v4();
-        const requestContext: APIGatewayEventRequestContext = <APIGatewayEventRequestContext> { requestId: apiRequestId };
-        const headerCorrelationId = v4();
-        const headers: Record<string, string> = { "X-Correlation-Id": headerCorrelationId};
-        const eventMock: APIGatewayProxyEvent = <APIGatewayProxyEvent> { queryStringParameters,  requestContext, headers };
-        const contextMock: Context = <Context> { };
+  test('createLogger() via header should return a logger object with the correct logFormat', () => {
+    const queryStringParameters: Record<string, string> = { message: 'Hello world!' };
+    const apiRequestId: string = v4();
+    const requestContext: APIGatewayEventRequestContext = <APIGatewayEventRequestContext> { requestId: apiRequestId };
+    const headerCorrelationId: string = v4();
+    const headers: Record<string, string> = { 'X-Correlation-Id': headerCorrelationId };
+    const eventMock: APIGatewayProxyEvent = <APIGatewayProxyEvent> { queryStringParameters, requestContext, headers };
+    const contextMock: Context = <Context> { };
 
-        const logger: Logger = createLogger(eventMock, contextMock);
+    const logger: Logger = createLogger(eventMock, contextMock);
 
-        expect(logger.logFormat).toBe(`{"apiRequestId":"${apiRequestId}","correlationId":"${headerCorrelationId}","message":"%s"}`);
-    });
+    expect(logger.logFormat).toBe(
+      `{"apiRequestId":"${apiRequestId}","correlationId":"${headerCorrelationId}","message":"%s"}`,
+    );
+  });
 
-    test("logger.debug() calls console.debug() with expected parameters", () => {
-        const logger: Logger = new Logger("", "");
-        console.debug = jest.fn();
+  test('logger.debug() calls console.debug() with expected parameters', () => {
+    const logger: Logger = new Logger('', '');
+    console.debug = jest.fn();
 
-        logger.debug("hello");
+    logger.debug('hello');
 
-        expect(console.debug).toHaveBeenCalledWith(logger.logFormat, "hello");
-    });
+    expect(console.debug).toHaveBeenCalledWith(logger.logFormat, 'hello');
+  });
 
-    test("logger.info() calls console.info() with expected parameters", () => {
-        const logger: Logger = new Logger("", "");
-        console.info = jest.fn();
+  test('logger.info() calls console.info() with expected parameters', () => {
+    const logger: Logger = new Logger('', '');
+    console.info = jest.fn();
 
-        logger.info("hello");
+    logger.info('hello');
 
-        expect(console.info).toHaveBeenCalledWith(logger.logFormat, "hello");
-    });
+    expect(console.info).toHaveBeenCalledWith(logger.logFormat, 'hello');
+  });
 
-    test("logger.warn() calls console.warn() with expected parameters", () => {
-        const logger: Logger = new Logger("", "");
-        console.warn = jest.fn();
+  test('logger.warn() calls console.warn() with expected parameters', () => {
+    const logger: Logger = new Logger('', '');
+    console.warn = jest.fn();
 
-        logger.warn("hello");
+    logger.warn('hello');
 
-        expect(console.warn).toHaveBeenCalledWith(logger.logFormat, "hello");
-    });
+    expect(console.warn).toHaveBeenCalledWith(logger.logFormat, 'hello');
+  });
 
-    test("logger.error() calls console.error() with expected parameters", () => {
-        const logger: Logger = new Logger("", "");
-        console.error = jest.fn();
+  test('logger.error() calls console.error() with expected parameters', () => {
+    const logger: Logger = new Logger('', '');
+    console.error = jest.fn();
 
-        logger.error("hello");
+    logger.error('hello');
 
-        expect(console.error).toHaveBeenCalledWith(logger.logFormat, "hello");
-    });
+    expect(console.error).toHaveBeenCalledWith(logger.logFormat, 'hello');
+  });
 });
