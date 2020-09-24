6766af0fcef26fb00ef20c558539c84a9dadc65a
Samuele Artuso <samuele.a@gmail.com>
2020-09-24 19:22:51 +0100

    fix: correctly generate a correlationId when lambda invoked with no headers

--- src/util/logger.ts
+++ src/util/logger.ts
@@ -27,7 +27,7 @@ export class Logger {
 export const createLogger = (event: APIGatewayProxyEvent, context: Context): Logger => {
   const lambdaRequestId: string = context.awsRequestId;
   const apiRequestId: string = event?.requestContext.requestId;
-  const correlationId: string = event?.headers['X-Correlation-Id'] || lambdaRequestId;
+  const correlationId: string = event?.headers?.['X-Correlation-Id'] || lambdaRequestId;
 
   return new Logger(apiRequestId, correlationId);
 };
