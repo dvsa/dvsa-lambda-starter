9dd9b5246eb61f8cc018223b08fd4e91a784e575
Joshua License <joshua.license@dvsa.gov.uk>
2022-04-13 09:36:58 +0100

    Simpilfy the logging logic

--- src/util/logger.ts
+++ src/util/logger.ts
@@ -2,24 +2,19 @@ import winston from 'winston';
 
 const logger = winston.createLogger({
   level: process.env.LOG_LEVEL,
+  format: winston.format.json(),
+  transports: [
+    new winston.transports.Console(),
+  ],
 });
 
-if (process.env.NODE_ENV === 'production') {
-  logger.add(new winston.transports.Console({
-    format: winston.format.combine(
-      winston.format.timestamp(),
-      winston.format.json(),
-    ),
-  }));
-}
-
+//
+// If not in production then log to the `console` with the format:
+// `${info.level}: ${info.message} JSON.stringify({ ...rest }) `
+//
 if (process.env.NODE_ENV !== 'production') {
   logger.add(new winston.transports.Console({
-    format: winston.format.combine(
-      winston.format.timestamp(),
-      winston.format.colorize(),
-      winston.format.simple(),
-    ),
+    format: winston.format.combine(winston.format.simple(), winston.format.colorize()),
   }));
 }
 
