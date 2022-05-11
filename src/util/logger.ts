5603cbc8563b582d75dd74778ebf3693bc6d8bd1
Joshua License <joshua.license@dvsa.gov.uk>
2022-05-11 12:43:41 +0100

    Bump winstonjs to fix metadata issue

--- src/util/logger.ts
+++ src/util/logger.ts
@@ -8,10 +8,8 @@ const logger = winston.createLogger({
   ],
 });
 
-//
 // If not in production then log to the `console` with the format:
-// `${info.level}: ${info.message} JSON.stringify({ ...rest }) `
-//
+// `${info.level}: ${info.message} JSON.stringify({ ...rest })`
 if (process.env.NODE_ENV !== 'production') {
   logger.add(new winston.transports.Console({
     format: winston.format.combine(winston.format.simple(), winston.format.colorize()),
