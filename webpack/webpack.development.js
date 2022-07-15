8e38a1c20cd1b2ca5bcc9b24457689ba755e860c
Joshua License <joshua.license@dvsa.gov.uk>
2022-07-15 09:01:57 +0100

    Copy environment variables into SAM containers

--- webpack/webpack.development.js
+++ webpack/webpack.development.js
@@ -1,7 +1,17 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
 
+const CopyPlugin = require('copy-webpack-plugin');
+const AwsSamPlugin = require('aws-sam-webpack-plugin');
+
 module.exports = merge(common, {
   mode: 'development',
   devtool: 'source-map',
+  plugins: [
+    new CopyPlugin({
+      patterns: Object.keys((new AwsSamPlugin({ vscodeDebug: false }).entry())).map((lambdaName) => ([
+        { from: './.env', to: `.aws-sam/build/${lambdaName}/`, noErrorOnMissing: true },
+      ])).flat()
+    }),
+  ]
 });
