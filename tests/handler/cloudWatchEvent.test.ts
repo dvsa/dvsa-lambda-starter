4c87f0d90b228decf44c3c3c6ef10e23755295b5
Joshua License <joshua.license@dvsa.gov.uk>
2022-08-30 11:59:42 +0100

    fixup! Turn on TypeScript strict mode

--- tests/handler/cloudWatchEvent.test.ts
+++ tests/handler/cloudWatchEvent.test.ts
@@ -6,11 +6,12 @@ jest.mock('../../src/util/logger.ts');
 
 describe('Test CloudWatch Event Lambda Function', () => {
   test('should return 200 with a success message', async () => {
-    const eventMock: ScheduledEvent = <ScheduledEvent> { id: v4() };
+    const id = v4();
+    const eventMock: ScheduledEvent = <ScheduledEvent> { id };
 
     const res: Record<string, string | number> = await handler(eventMock);
 
     expect(res.statusCode).toBe(200);
-    expect(res.body).toEqual('Cloudwatch event successfully triggered!');
+    expect(res.body).toEqual(`Cloudwatch event ${id} successfully triggered!`);
   });
 });
